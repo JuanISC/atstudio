e94cbeafaa1b632a60d97bab08595ece6a7518cc
JuanISC <jglez.isc@gmail.com>
2024-01-22 16:07:51 -0600

    first commit

---
+++ src/app/card-img/card-img.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit,Input } from '@angular/core';
+
+@Component({
+  selector: 'app-card-img',
+  templateUrl: './card-img.component.html',
+  styleUrls: ['./card-img.component.css']
+})
+export class CardImgComponent implements OnInit {
+
+  @Input() title = ''; 
+  @Input() text = ''; 
+  @Input() srcImg = ''; 
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
