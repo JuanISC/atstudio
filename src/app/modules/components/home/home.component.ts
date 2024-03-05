5898caa2546b167dfa30cf76d9e5fce989bfcfff
JuanISC <jglez.isc@gmail.com>
2024-03-05 15:36:21 -0600

    update url hsl video player

--- src/app/modules/components/home/home.component.ts
+++ src/app/modules/components/home/home.component.ts
@@ -9,28 +9,48 @@ declare var Hls;
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit, AfterViewInit {
-  @ViewChild('videohls2') videoElementRef!: ElementRef;
-  videoElement!: HTMLVideoElement;
+  @ViewChild('videohls1') videoHls1!: ElementRef;
+  @ViewChild('videohls2') videoHls2!: ElementRef;
+  @ViewChild('videohls3') videoHls3!: ElementRef;
+  @ViewChild('videohls4') videoHls4!: ElementRef;
+  @ViewChild('videohls5') videoHls5!: ElementRef;
+  @ViewChild('videohls6') videoHls6!: ElementRef;
 
   constructor() { }
 
   ngOnInit(): void {
   }
-
   ngAfterViewInit(): void {
-    this.videoElement = this.videoElementRef?.nativeElement;
+    this.loadSourceVideoHLS(this.videoHls1,'https://d1piva4jon12ui.cloudfront.net/introduce/modulo3_6.m3u8');
+    this.loadSourceVideoHLS(this.videoHls4,'https://d1piva4jon12ui.cloudfront.net/hls/introduce/modulo3_6.m3u8');
+    this.loadSourceVideoHLS(this.videoHls5,'https://d1piva4jon12ui.cloudfront.net/video/introduce/modulo3_6.m3u8');
+    this.loadSourceVideoHLS(this.videoHls6,'https://d1a79jkkzk4r01.cloudfront.net/introduce/modulo3_6.m3u8');
+    
+    
+    this.loadSourceVideoHLS(this.videoHls2,'https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8');
+    this.loadSourceVideoHLS(this.videoHls3,'https://cdn.jwplayer.com/manifests/pZxWPRg4.m3u8');
+
+    // this.loadSourceVideoHLS(this.videoHls1,'/assets/video/introduce/modulo3_6.m3u8');
+    // this.loadSourceVideoHLS(this.videoHls2,'/assets/video/introduce/modulo3_6.m3u8');
+  }
+
+  loadSourceVideoHLS(videoElementRef: ElementRef,urlSource: String ): void {
+    var videoElement!: HTMLVideoElement;
+
+    videoElement = videoElementRef?.nativeElement;
 
     if (Hls.isSupported()) {
       console.log("Video streaming supported by HLSjs")
       console.log(Hls);
-      console.log(this.videoElement);
+      console.log(videoElement);
 
       var hls = new Hls();
       // hls.loadSource('/assets/video/d7251d6260de4f0192f374ddc98b772d/modulo3_6.m3u8');
-      hls.loadSource('https://d1a79jkkzk4r01.cloudfront.net/introduce/introduce.m3u8');
-      hls.attachMedia(this.videoElement);
+      // hls.loadSource('https://d1a79jkkzk4r01.cloudfront.net/introduce/introduce.m3u8');
+      hls.loadSource(urlSource);
+      hls.attachMedia(videoElement);
       hls.on(Hls.Events.MANIFEST_PARSED, () => {
-        // this.videoElement.play();
+        // videoElement.play();
         console.log("1");
       });
       hls.on(Hls.Events.ERROR, (event, data) => {
@@ -53,11 +73,9 @@ export class HomeComponent implements OnInit, AfterViewInit {
           }
         }
       });
-    }
-
-    else if (this.videoElement.canPlayType('application/vnd.apple.mpegurl')) {
+    }else if (videoElement.canPlayType('application/vnd.apple.mpegurl')) {
       // alert("You're using an Apple product!");
-      this.videoElement.src = 'https://bitdash-a.akamaihd.net/content/sintel/hls/playlist.m3u8';
+      videoElement.src = 'https://bitdash-a.akamaihd.net/content/sintel/hls/playlist.m3u8';
       console.log("2");
     }
   }
